b0e2118c6d82fb76b5b48872687f13bebacf85bf
Tom Sitter <thomas.sitter@gmail.com>
2021-12-15 13:18:48 -0800

    Add day 9 part 2

--- day_9/day9.py
+++ day_9/day9.py
@@ -1,15 +1,26 @@
 import click
 import numpy as np
+from itertools import chain
 
 def adjacent(pos, data):
     """Returns a list of adjacent values from the data matrix for the given position"""
     x,y = pos
+    x_size, y_size = data.shape
     for (adj_x, adj_y) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
-        try:
-            if adj_x >= 0 and adj_y >= 0:
-                yield data[adj_x][adj_y]
-        except IndexError:
-            pass
+        if adj_x >= 0 and adj_x < x_size and adj_y >= 0 and adj_y < y_size:
+            yield (adj_x, adj_y)
+
+def find_basin(pos, data):
+    x,y = pos
+    yield (x,y)
+    x_size, y_size = data.shape
+    for (adj_x, adj_y) in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+        if adj_x >= 0 and adj_x < x_size and adj_y >= 0 and adj_y < y_size:
+            adj_pt = data[adj_x][adj_y]
+            if data[x][y] < adj_pt and adj_pt < 9:
+                yield (adj_x, adj_y)
+                yield from find_basin((adj_x, adj_y), data)
+
     
 @click.command()
 @click.option('--example', is_flag=True, help='Run example input')
@@ -25,20 +36,25 @@ def run(example):
     with open(filename, 'r') as infile:
         data = []
         for row in infile:
-            data.append([int(num) for num in row.strip()])
+            data.append([num for num in row.strip()])
 
-        list(adjacent((2, 2), data))
-
-        #data = np.array(data, dtype='int') 
+        data = np.array(data, dtype='int')
         low_points = []
+        basins = []
         for x, row in enumerate(data):
             for y, point in enumerate(row):
-                if all(point < adj_point for adj_point in adjacent((x, y), data)):
+                if all(point < data[adj_x, adj_y] for (adj_x, adj_y) in adjacent((x, y), data)):
                     low_points.append(point)
+                basins.append(list(find_basin((x,y), data)))
 
         risk_levels = [1+point for point in low_points]
-
         print(f'Solution 1: {sum(risk_levels)}')
+
+        top_3 = sorted([set(b) for b in basins], key=len, reverse=True)[0:3]
+        product = np.prod([len(t) for t in top_3])
+        print(f'Solution 2: {product}')
+
+        
 
 if __name__ == '__main__':
     run()
